3b34f00a221d58951abd9759efaa8a63d1ac84f2
Tristan Trosvig <ttrosvig@Tristans-MacBook-Air.local>
2020-08-28 15:34:36 -0500

    added an api that fetches a random dessert recipe

---
+++ static/app.js
@@ -0,0 +1,18 @@
+$("#get-recipe").on("click", async function () {
+    resp = await axios.get('https://api.spoonacular.com/recipes/random?tags=dessert&number=1&apiKey=76d7a5ae655a48daab0f3fcbd50a5a28');
+
+    title = resp.data.recipes[0].title;
+
+    url = resp.data.recipes[0].sourceUrl;
+
+    $info = $("#information-area");
+
+    $info.empty();
+
+    $info.append(title);
+    
+    $link = $("<a>View Recipe</a>");
+    $("#link-area").append($link);
+
+    $link.attr("href", url)
+})
